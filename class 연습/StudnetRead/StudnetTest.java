02d8284209f58d68cc273f64ec76077348e0bb4e
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-08 17:23:22 +0900

    class 연습3 ArrayList(class)2

---
+++ class 연습/StudnetRead/StudnetTest.java
@@ -0,0 +1,24 @@
+package StudnetRead;
+
+public class StudnetTest {
+    public static void main(String[] args) {
+        Student lee=new Student("lee");
+        Student Kim=new Student("kim");
+        Student cho=new Student("cho");
+
+        lee.addBook("태백산맥 1");
+        lee.addBook("태백산맥 2");
+        Kim.addBook("토지 1");
+        Kim.addBook("토지 2");
+        Kim.addBook("토지 3");
+        cho.addBook("해리포터 1");
+        cho.addBook("해리포터 2");
+        cho.addBook("해리포터 3");
+        cho.addBook("해리포터 4");
+        cho.addBook("해리포터 5");
+        cho.addBook("해리포터 6");
+        lee.show();
+        Kim.show();
+        cho.show();
+    }
+}
