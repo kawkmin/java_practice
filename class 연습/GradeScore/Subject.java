31c5b7e3a947e5c193e13e786cc55433b7614b4a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-08 16:59:43 +0900

    class복습2 ArrayList<class>

---
+++ class 연습/GradeScore/Subject.java
@@ -0,0 +1,19 @@
+package GradeScore;
+
+public class Subject {
+    private String Name;
+    private int score;
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Subject(String name, int score) {
+        Name = name;
+        this.score = score;
+    }
+}
