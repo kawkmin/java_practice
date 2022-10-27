383c955c4e8dcd35bee5ef891fada51752bc0501
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-10-27 15:50:54 +0900

    import 인텔리제이

--- Class remind/src/customer/test.java
+++ Class remind/src/customer/test.java
@@ -21,13 +21,15 @@ public class test {
         for(Customer customer:customerArrayList){
             System.out.println(customer.showinfo()+" 지불금액은 "+customer.calcPrice(10000)+"원 입니다");
         }
-        /*int pricekim=customerkim.calcPrice(10000);
+        /*
+        int pricekim=customerkim.calcPrice(10000);
         int pricepark=vipCustomerpark.calcPrice(10000);//같은 메서드 이름이지만 따로존재. 인스턴스의 메소드의 주소 호출(vip꺼는 가상 메서드)
         int pricegawk=goldCustomergawk.calcPrice(10000);
 
         System.out.println(customerkim.showinfo()+"지불금액은 "+pricekim+"원 입니다");
         System.out.println(vipCustomerpark.showinfo()+"지불금액은 "+pricepark+"원 입니다");
-        System.out.println(goldCustomergawk.showinfo()+"지불금액은 "+pricegawk+"원 입니다");*/
+        System.out.println(goldCustomergawk.showinfo()+"지불금액은 "+pricegawk+"원 입니다");
+        */
 
     }
 }
