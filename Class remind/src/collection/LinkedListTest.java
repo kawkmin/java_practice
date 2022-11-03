b4058102b9866b57a9b4b00708417eba4cc3d0cd
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-03 11:26:11 +0900

    [feat] LinkedList 연습

---
+++ Class remind/src/collection/LinkedListTest.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList=new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("B");
+        linkedList.add("C");
+        System.out.println(linkedList);
+        linkedList.add(1,"D");
+        System.out.println(linkedList);
+        linkedList.removeLast();
+
+        for(int i=0;i<linkedList.size();i++){
+            System.out.println(linkedList.get(i));
+        }
+    }
+}
