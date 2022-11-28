d54e39bb8bdc24f80e5e53bf92517ac2bfe78e9a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-28 19:51:00 +0900

    feat: 이름을 가진 자동차들의 객체 만들기 구현

--- OOP_Prcatice/java-racingcar-precourse-master/src/main/java/model/Cars.java
+++ OOP_Prcatice/java-racingcar-precourse-master/src/main/java/model/Cars.java
@@ -1,8 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
-    private List<Car> carList;
+    private List<Car> carList=new ArrayList<>();
 
+    public void addCar(String carName){
+        Arrays.asList(carName.split(",")).stream()
+                .forEach(name->carList.add(new Car(name)));
+    }
 }
