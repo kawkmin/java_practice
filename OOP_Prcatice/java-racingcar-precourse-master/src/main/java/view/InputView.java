292bff3249c78e7ee09a01f458c2d8fd41ab3de2
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-28 19:46:53 +0900

    feat: 자동차 이름을 입력받는 메서드 구현

--- OOP_Prcatice/java-racingcar-precourse-master/src/main/java/view/InputView.java
+++ OOP_Prcatice/java-racingcar-precourse-master/src/main/java/view/InputView.java
@@ -1,4 +1,10 @@
 package view;
 
+import static view.ViewEnum.INPUT_CAR_NAME;
+
 public class InputView {
+    public static String getCarName(){
+        System.out.println(INPUT_CAR_NAME.getMessage());
+        return System.console().readLine();
+    }
 }
