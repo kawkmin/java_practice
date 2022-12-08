1f31e2fd1b4baed4cc4a9079bb669d8397ab9075
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-08 19:48:44 +0900

    create

--- OOP_Prcatice/java-racingcar-precourse-master/src/main/java/Application.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-    }
-}
