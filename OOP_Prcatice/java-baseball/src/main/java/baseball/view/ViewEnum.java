464adc0204bef6100f2d27f312106065d23776f9
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-10 17:39:17 +0900

    feat: Enum을 통해서 View 메시지 상수 관리

---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/view/ViewEnum.java
@@ -0,0 +1,20 @@
+package baseball.view;
+
+public enum ViewEnum {
+    INPUT_USER_NUMBERS_MESSAGE("숫자를 입력해주세요 : "),
+    INPUT_RESTART_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+
+    OUTPUT_GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
+    OUTPUT_GAME_END_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    ;
+
+    private String message;
+
+    ViewEnum(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
