3d63b764bf8649a53ca5f90f79431f7aa4e6125b
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-10 16:56:47 +0900

    feat: 컴퓨터의 숫자를 가진 컴퓨터 객체 생성

---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/model/Computer.java
@@ -0,0 +1,15 @@
+package baseball.model;
+
+import baseball.util.CreateRandomNum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computerNum = new ArrayList<>();
+
+    public Computer(List<Integer> computerNum) {
+        this.computerNum = computerNum;
+    }
+
+}
