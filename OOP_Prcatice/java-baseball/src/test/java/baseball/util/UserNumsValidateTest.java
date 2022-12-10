63337d5bad75a34f0a6ec096162f267e1977fb6d
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-10 19:08:30 +0900

    test: 사용자의 숫자가 3개 이상일 때 에외처리 확인 테스트

---
+++ OOP_Prcatice/java-baseball/src/test/java/baseball/util/UserNumsValidateTest.java
@@ -0,0 +1,17 @@
+package baseball.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserNumsValidateTest {
+    @DisplayName("유저 숫자가 3개 이상일 때 예외 처리 되는지 확인")
+    @Test
+    void checkUserNumsSize(){
+        assertThatThrownBy(()->UserNumsValidate.checkUserNums("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
