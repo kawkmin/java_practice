9cdc8b6fd1a82baa4c7c4147b2c6ed204116c34d
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-12 16:49:34 +0900

    test: 스트라이크 계산이 정상적인지 테스트

--- OOP_Prcatice/java-baseball/src/test/java/baseball/model/JudgementTest.java
+++ OOP_Prcatice/java-baseball/src/test/java/baseball/model/JudgementTest.java
@@ -10,7 +10,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JudgementTest {
-
     @DisplayName("볼 계산이 정상적인지 확인")
     @Test
     void checkBallCount(){
@@ -19,5 +18,15 @@ class JudgementTest {
         Judgement judgement=new Judgement(user,computer);
         judgement.calculator();
         assertThat(judgement.getBallCount()).isEqualTo(2);
+    }
+
+    @DisplayName("스트라이크 계산이 정상적인지 확인")
+    @Test
+    void checkStrikeCount(){
+        User user=new User("123");
+        Computer computer=new Computer(List.of(2,1,3));
+        Judgement judgement=new Judgement(user,computer);
+        judgement.calculator();
+        assertThat(judgement.getStrikeCount()).isEqualTo(1);
     }
 }
