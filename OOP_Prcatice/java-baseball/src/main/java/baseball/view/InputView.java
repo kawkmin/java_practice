313ce5627a9e917632499dcc94bc4035d3ba6797
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-12 17:15:31 +0900

    feat: 재시작 여부를 확인하는 숫자 입력받기 구현

--- OOP_Prcatice/java-baseball/src/main/java/baseball/view/InputView.java
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/view/InputView.java
@@ -3,6 +3,7 @@ package baseball.view;
 import baseball.util.UserNumsValidate;
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.view.ViewEnum.INPUT_RESTART_MESSAGE;
 import static baseball.view.ViewEnum.INPUT_USER_NUMBERS_MESSAGE;
 
 public class InputView {
@@ -11,5 +12,10 @@ public class InputView {
     public String readUserNumber(){
         System.out.print(INPUT_USER_NUMBERS_MESSAGE.getMessage());
         return UserNumsValidate.checkUserNums(Console.readLine());
+    }
+
+    public String readRestartNumber(){
+        System.out.println(INPUT_RESTART_MESSAGE.getMessage());
+        return Console.readLine();
     }
 }
