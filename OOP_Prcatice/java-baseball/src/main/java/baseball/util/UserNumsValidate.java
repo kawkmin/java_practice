5d163b28a2778363cf96d9d0281828d561aa484a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-12 14:57:44 +0900

    refactor: 오류 메시지를 ENUM을 통해서 관리

--- OOP_Prcatice/java-baseball/src/main/java/baseball/util/UserNumsValidate.java
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/util/UserNumsValidate.java
@@ -6,10 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static baseball.util.ErrorEnum.*;
+
 public class UserNumsValidate {
     private static final int USER_NUMS_SIZE=3;
-    private static final int MAX_NUM=9;
-    private static final int NIM_NUM=1;
+    private static final char MAX_NUM='9';
+    private static final char NIM_NUM='1';
 
     public static String checkUserNums(String userNums){
         checkDistinct(userNums);
@@ -20,14 +22,14 @@ public class UserNumsValidate {
 
     private static void checkSize(String userNums){
         if(userNums.length()!=USER_NUMS_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(USER_SIZE_ERROR.getMessage());
         }
     }
 
     private static void checkRight(String userNums){
         for(int i=0;i<userNums.length();i++){
             if(userNums.charAt(i)>MAX_NUM||userNums.charAt(i)<NIM_NUM){
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(USER_RIGHT_ERROR.getMessage());
             }
         }
     }
@@ -36,7 +38,7 @@ public class UserNumsValidate {
         List<String> list= Arrays.asList(userNums.split(""));
         list=list.stream().distinct().collect(Collectors.toList());
         if(list.size()!=USER_NUMS_SIZE){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(USER_DISTINCT_ERROR.getMessage());
         }
     }
 }
