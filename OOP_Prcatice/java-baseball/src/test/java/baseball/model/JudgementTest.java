86d3fa36a0c3ac51b286a4f1f1486e6126dc4486
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-12 16:47:34 +0900

    test: 볼 계산이 정상적인지 테스트

---
+++ OOP_Prcatice/java-baseball/src/test/java/baseball/model/JudgementTest.java
@@ -0,0 +1,23 @@
+package baseball.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JudgementTest {
+
+    @DisplayName("볼 계산이 정상적인지 확인")
+    @Test
+    void checkBallCount(){
+        User user=new User("123");
+        Computer computer=new Computer(List.of(2,1,3));
+        Judgement judgement=new Judgement(user,computer);
+        judgement.calculator();
+        assertThat(judgement.getBallCount()).isEqualTo(2);
+    }
+}
