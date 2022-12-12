8bc8cd2e0550dabac3e24c571594be949f5d45d6
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-12 14:58:15 +0900

    feat: 게임을 관리하는 컨트롤러 생성

---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,22 @@
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.User;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    public GameController() {
+        inputView=new InputView();
+        outputView=new OutputView();
+    }
+
+    public void Start(){
+        try{
+        }catch (IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
