4b6e0356ddd3cb62aa1bdc08ab0adda791231bc5
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 20:03:25 +0900

    feat: 당첨 번호에 중복이 있으면 예외 처리

--- OOP_Prcatice/java-lotto/src/main/java/lotto/utils/WinLottoValidator.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/utils/WinLottoValidator.java
@@ -1,21 +1,38 @@
 package lotto.utils;
 
-import java.util.List;
+import lotto.model.Lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lotto.utils.ErrorEnum.WIN_LOTTO_DISTINCT_ERROR;
 import static lotto.utils.ErrorEnum.WIN_LOTTO_SIZE_ERROR;
 
 public class WinLottoValidator {
+    private static final int LOTTO_SIZE=6;
+
     public WinLottoValidator() {
     }
 
     public String checkWinLotto(String winLotto){
         checkSize(winLotto);
+        checkDistinct(winLotto);
         return winLotto;
     }
 
     private void checkSize(String winLotto){
-        if(winLotto.split(",").length!=6){
+        if(winLotto.split(",").length!=LOTTO_SIZE){
             throw new IllegalArgumentException(WIN_LOTTO_SIZE_ERROR.getMessage());
+        }
+    }
+
+    private void checkDistinct(String winLotto){
+        List<String> lotto= Arrays.asList(winLotto.split(","));
+        lotto= lotto.stream().distinct().collect(Collectors.toList());
+
+        if(lotto.size()!=LOTTO_SIZE){
+            throw new IllegalArgumentException(WIN_LOTTO_DISTINCT_ERROR.getMessage());
         }
     }
 }
