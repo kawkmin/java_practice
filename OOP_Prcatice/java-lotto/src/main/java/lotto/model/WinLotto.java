9dc428ddae26ebe5b80a5308a23aa54f06fcfb4e
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 20:35:27 +0900

    test: 당첨 로또번호를 정수형 리스트로 받도록 구현

--- OOP_Prcatice/java-lotto/src/main/java/lotto/model/WinLotto.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/model/WinLotto.java
@@ -1,4 +1,25 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WinLotto {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinLotto(String winningLotto, String bonusNumber) {
+        this.winningLotto = addWinningLotto(winningLotto);
+        this.bonusNumber = Integer.parseInt(bonusNumber);
+    }
+
+    public Lotto addWinningLotto(String winningLotto){
+        List<String> winningLottoList= Arrays.asList(winningLotto.split(","));
+        List<Integer> LottoNumber=new ArrayList<>();
+        winningLottoList.stream()
+                .mapToInt(Integer::parseInt)
+                .forEach(number->LottoNumber.add(number));
+
+        return new Lotto(LottoNumber);
+    }
 }
