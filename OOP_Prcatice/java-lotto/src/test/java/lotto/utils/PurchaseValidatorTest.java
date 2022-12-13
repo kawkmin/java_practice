dbeefacb6b54805a212bf0948863c8d54458b345
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 17:23:22 +0900

    test: 구입 금액이 정상일 때 예외처리 안되는지 확인

--- OOP_Prcatice/java-lotto/src/test/java/lotto/utils/PurchaseValidatorTest.java
+++ OOP_Prcatice/java-lotto/src/test/java/lotto/utils/PurchaseValidatorTest.java
@@ -11,11 +11,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class PurchaseValidatorTest {
     private final PurchaseValidator purchaseValidator=new PurchaseValidator();
 
+    @DisplayName("구입 금액이 정상적일 때 예외가 발생하지 않는다.")
+    @Test
+    void testRight(){
+        assertThat(purchaseValidator.checkPurchase("5000"));
+    }
+
     @DisplayName("구입 금액이 숫자가 아니면 예외가 발생한다.")
     @Test
     void testNumber() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 () -> purchaseValidator.checkPurchase("abc") );
         assertThat(exception.getMessage()).isEqualTo("[ERROR] 숫자만 입력해 주세요.");
+    }
+
+    @DisplayName("구입 금액이 1000단위가 아니면 예외가 발생한다.")
+    @Test
+    void testUnit() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> purchaseValidator.checkPurchase("10001") );
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] 1000원 단위로 입력해 주세요.");
     }
 }
