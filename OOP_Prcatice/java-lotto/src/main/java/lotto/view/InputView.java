fd12448917c9b21ef315833e8491605e29f42af5
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 19:40:01 +0900

    feat: 당첨 번호를 입력받도록 구현

--- OOP_Prcatice/java-lotto/src/main/java/lotto/view/InputView.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/view/InputView.java
@@ -12,8 +12,13 @@ public class InputView {
         purchaseValidator=new PurchaseValidator();
     }
 
-    public String purchaseMoney(){
+    public String readPurchaseMoney(){
         System.out.println(INPUT_PURCHASE_MONEY.getMessage());
         return purchaseValidator.checkPurchase(Console.readLine());
+    }
+
+    public String readWinLotto(){
+        System.out.println(INPUT_WIN_LOTTO.getMessage());
+        return Console.readLine();
     }
 }
--- OOP_Prcatice/java-lotto/src/main/java/lotto/view/ViewEnum.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/view/ViewEnum.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 public enum ViewEnum {
     INPUT_PURCHASE_MONEY("구입금액을 입력해 주세요."),
+    INPUT_WIN_LOTTO("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
 
     OUTPUT_PURCHASE_LOTTO("\n%d개를 구매했습니다.\n"),
     ;
