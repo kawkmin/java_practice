b76973d2eb48fec97dd43a8a281937d6db804040
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 17:00:09 +0900

    feat: 제너레이터가 구매 금액을 가지도록 구현

--- OOP_Prcatice/java-lotto/src/main/java/lotto/model/LottoGenerator.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/model/LottoGenerator.java
@@ -1,4 +1,9 @@
 package lotto.model;
 
 public class LottoGenerator {
+    private final int purchaseMoney;
+
+    public LottoGenerator(int purchaseMoney) {
+        this.purchaseMoney=purchaseMoney;
+    }
 }
