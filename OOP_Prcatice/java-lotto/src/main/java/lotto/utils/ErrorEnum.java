d58ff5f5d61dcd22758f20df726c56e0dbde2ffc
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 20:05:46 +0900

    test: 당첨 번호에 중복이 있으면 예외 처리되는지 테스트

--- OOP_Prcatice/java-lotto/src/main/java/lotto/utils/ErrorEnum.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/utils/ErrorEnum.java
@@ -4,7 +4,8 @@ public enum ErrorEnum {
     MONEY_NUMBER_ERROR("숫자만 입력해 주세요."),
     MONEY_UNIT_ERROR("1000원 단위로 입력해 주세요."),
 
-    WIN_LOTTO_SIZE_ERROR("6개를 입력해 주세요")
+    WIN_LOTTO_SIZE_ERROR("6개를 입력해 주세요"),
+    WIN_LOTTO_DISTINCT_ERROR("중복된 숫자는 입력할 수 없습니다")
     ;
 
     private static final String ERROR="[ERROR] ";
--- OOP_Prcatice/java-lotto/src/test/java/lotto/utils/WinLottoValidatorTest.java
+++ OOP_Prcatice/java-lotto/src/test/java/lotto/utils/WinLottoValidatorTest.java
@@ -16,4 +16,12 @@ class WinLottoValidatorTest {
                 ()-> winLottoValidator.checkWinLotto("1,2,3,4,5,6,7"));
         assertThat(exception.getMessage()).isEqualTo("[ERROR] 6개를 입력해 주세요");
     }
+
+    @DisplayName("당첨 번호가 중복이 있으면 예외처리 되는지 테스트")
+    @Test
+    void testDistinct(){
+        IllegalArgumentException exception=assertThrows(IllegalArgumentException.class,
+                ()-> winLottoValidator.checkWinLotto("1,2,3,4,5,5"));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] 중복된 숫자는 입력할 수 없습니다");
+    }
 }
