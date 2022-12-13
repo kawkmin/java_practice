6989738d07e8ad7f6f80a841be856fd038165a79
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 17:42:12 +0900

    feat: 구입 갯수 만큼 lotto 리스트 생성 구현

--- OOP_Prcatice/java-lotto/src/main/java/lotto/model/LottoGenerator.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/model/LottoGenerator.java
@@ -1,9 +1,32 @@
 package lotto.model;
 
+import lotto.utils.CreateRandomLottoNumber;
+import lotto.utils.PurchaseValidator;
+
+import java.util.List;
+
 public class LottoGenerator {
+    private final int moneyUnit=1000;
     private final int purchaseMoney;
+    private final int cnt;
+
+    private List<Lotto> purchaseLottos;
 
     public LottoGenerator(String purchaseMoney) {
         this.purchaseMoney=Integer.parseInt(purchaseMoney);
+        this.cnt=this.purchaseMoney/moneyUnit;
+    }
+
+    private void purchaseLotto(){
+        int cnt=this.cnt;
+
+        while(cnt>0){
+            addLotto(CreateRandomLottoNumber.createNumber());
+            cnt--;
+        }
+    }
+
+    private void addLotto(List<Integer> lottoNumbers){
+        this.purchaseLottos.add(new Lotto(lottoNumbers));
     }
 }
