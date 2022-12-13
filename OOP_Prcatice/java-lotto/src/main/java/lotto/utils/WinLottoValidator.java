2a376f65c03b285a8d87271c11f7ed4cc1ec5db4
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-12-13 19:43:57 +0900

    feat: 당첨 번호를 예외 처리 하는 클래스 생성

---
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/utils/WinLottoValidator.java
@@ -0,0 +1,10 @@
+package lotto.utils;
+
+public class WinLottoValidator {
+    public WinLottoValidator() {
+    }
+
+    public String checkWinLotto(String winLotto){
+        return winLotto;
+    }
+}
