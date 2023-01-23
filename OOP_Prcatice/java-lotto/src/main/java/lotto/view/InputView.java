9e2c13c2a1f9b312e2ffc313e840ec2c7f879d54
kawksung <alstjd5479@naver.com>
2023-01-23 16:23:14 +0900

    feat: 당첨 로또와 보너스 번호 받도록 구현 + 보너스 번호 중복 확인

--- OOP_Prcatice/java-lotto/src/main/java/lotto/view/InputView.java
+++ OOP_Prcatice/java-lotto/src/main/java/lotto/view/InputView.java
@@ -2,14 +2,17 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.utils.PurchaseValidator;
+import lotto.utils.WinLottoValidator;
 
 import static lotto.view.ViewEnum.*;
 
 public class InputView {
     private PurchaseValidator purchaseValidator;
+    private WinLottoValidator winLottoValidator;
 
     public InputView() {
         purchaseValidator=new PurchaseValidator();
+        winLottoValidator=new WinLottoValidator();
     }
 
     public String readPurchaseMoney(){
@@ -19,11 +22,11 @@ public class InputView {
 
     public String readWinLotto(){
         System.out.println(INPUT_WIN_LOTTO.getMessage());
-        return Console.readLine();
+        return winLottoValidator.checkWinLotto(Console.readLine());
     }
 
     public String readWinLottoOfBonusNumber(){
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println(INPUT_BONUS_NUMBER.getMessage());
         return Console.readLine();
     }
 }
