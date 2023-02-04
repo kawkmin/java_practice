2363ae4ff32275e937d938c282b6e8c567ea1518
kawksung <alstjd5479@naver.com>
2023-02-04 15:49:01 +0900

    feat: Controller 생성 및 실행 파일과 연결

--- OOP_Prcatice/java-baseball/src/main/java/baseball/Application.java
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/Application.java
@@ -1,8 +1,11 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+
 public class Application {
 
   public static void main(String[] args) {
-
+    BaseBallController baseBallController = new BaseBallController();
+    baseBallController.gameStart();
   }
 }
---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/controller/BaseBallController.java
@@ -0,0 +1,15 @@
+package baseball.controller;
+
+import baseball.view.OutputView;
+
+public class BaseBallController {
+
+  private final OutputView outputView = new OutputView();
+
+  public BaseBallController() {
+  }
+
+  public void gameStart() {
+
+  }
+}
