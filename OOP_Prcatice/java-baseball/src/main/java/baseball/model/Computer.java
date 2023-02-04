549f9eded428768705b8ac1703999f2a1601d67a
kawksung <alstjd5479@naver.com>
2023-02-04 15:11:51 +0900

    feat: 숫자 리스트를 가진 Computer 모델 생성

---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/model/Computer.java
@@ -0,0 +1,12 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+  private final List<Integer> Number;
+
+  public Computer(List<Integer> number) {
+    Number = number;
+  }
+}
