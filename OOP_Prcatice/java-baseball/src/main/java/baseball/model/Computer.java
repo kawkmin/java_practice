df20e1e3af622b08bf76b46323c6d85e92247288
kawksung <alstjd5479@naver.com>
2023-02-04 15:31:29 +0900

    feat: 컴퓨터가 1부터 9까지의 서로 다른 임의의 수 3개를 가지도록 구현

--- OOP_Prcatice/java-baseball/src/main/java/baseball/model/Computer.java
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/model/Computer.java
@@ -1,12 +1,14 @@
 package baseball.model;
 
+import baseball.util.RandomUtil;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
+
   private final List<Integer> Number;
 
-  public Computer(List<Integer> number) {
-    Number = number;
+  public Computer() {
+    this.Number = RandomUtil.makeRandomNumbers();
   }
 }
