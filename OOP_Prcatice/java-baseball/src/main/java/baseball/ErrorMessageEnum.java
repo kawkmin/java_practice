c273f0ebe79b85d86e0fe26d05491ff8970563c9
kawksung <alstjd5479@naver.com>
2023-02-04 17:53:47 +0900

    feat: error 메시지를 담은 Enum 구현

---
+++ OOP_Prcatice/java-baseball/src/main/java/baseball/ErrorMessageEnum.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public enum ErrorMessageEnum {
+  ERROR_USER_NUMBER_SIZE("3개를 입력해 주세요"),
+  ERROR_USER_NUMBER_RIGHT("1에서 9의 숫자만 입력해 주세요"),
+  ERROR_USER_NUMBER_DISTINCT("중복되지 않는 숫자를 입력해 주세요"),
+  ;
+
+  private static final String ERROR_MESSAGE = "[ERROR] ";
+  private String message;
+
+  ErrorMessageEnum(String message) {
+    this.message = ERROR_MESSAGE + message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
